b34b5e5e488b993fb23f2398118d6364dae7088b
Gopal Dahale <gopaldahale00@gmail.com>
2024-09-12 20:17:45 +0530

    ready go

--- src/components/News.js
+++ src/components/News.js
@@ -1,26 +1,26 @@
-// import React, { useEffect, useState } from 'react';
-import React from 'react';
-// import NewsItem from './NewsItem';
-// import placeholderImage from '../img/image-placeholder.jpg';
+import React, { useEffect, useState } from 'react';
+// import React from 'react';
+import NewsItem from './NewsItem';
+import placeholderImage from '../img/image-placeholder.jpg';
 export default function NewsCompo() {
 
 
-  // const [articles, setArticles] = useState([]);
-  // useEffect(() => {
-  //   const url = 'https://newsapi.org/v2/top-headlines?country=us&apiKey=9cb45c816ff64e5993f4498a1564e2c1';
+  const [articles, setArticles] = useState([]);
+  useEffect(() => {
+    const url = 'https://newsapi.org/v2/top-headlines?country=us&apiKey=9cb45c816ff64e5993f4498a1564e2c1';
 
-  //   fetch(url)
-  //     .then((response) => response.json())
-  //     .then((data) => setArticles(data.articles))
-  //     .catch((error) => console.error('Error:', error));
-  // }, []);
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => setArticles(data.articles))
+      .catch((error) => console.error('Error:', error));
+  }, []);
 
 
   return (
     <>
-      <h2>Today's Hatake News</h2>
+      <h2>Today's Hatake Newss</h2>
       <div className="row g-4">
-        {/* { articles.length > 0 ? (
+        { articles.length > 0 ? (
         articles.map((article, index) => (
           <div className="col-lg-3 col-md-6 col-sm-12" key={index}>
             <NewsItem imgUrl={ article.urlToImage ? article.urlToImage : placeholderImage } title={article.title.slice(0,40)} description={typeof article.description === 'string' ? (article.description.length > 40 ? article.description.slice(0, 88) + '...' : article.description) : ''} newsUrl={article.id} />
@@ -30,7 +30,7 @@ export default function NewsCompo() {
       ) : (
         <p>Loading...</p>
       )
-      } */}
+      }
       </div>
     </>
   )
