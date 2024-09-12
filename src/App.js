9f132bf0e87f3a22846764242e1fedb3be939108
Gopal Dahale <gopaldahale00@gmail.com>
2024-09-12 20:14:02 +0530

    ready

--- src/App.js
+++ src/App.js
@@ -1,18 +1,8 @@
 import './App.css';
 import Navbar from './components/Navbar';
-// import News from './components/PractiveNews';
 import News from './components/News';
-// import React from 'react';
-// import React, { useEffect, useState } from 'react';
 
 export default function App() {
-  // var url = 'https://jsonplaceholder.typicode.com/users'
-
-  // var req = new Request(url);
-
-  // fetch(req).then(function (response) {
-  //   console.log(response.json());
-  // })
 
 
   return (
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,17 +2,17 @@ import React from 'react'
 
 export default function NewsItem(props) {           
   return (
-    <>
-        <div className="card news-item" >
-          <div className='card-thumbnail-img'>
-            <img src={props.imgUrl} className="card-img-top " alt="..."/>
-          </div>
-            <div className="card-body">
-                <h5 className="card-title">{props.title}</h5>
-                <p className="card-text">{props.description}</p>
-                <a href={props.newsUrl} className="btn btn-primary read-more-btn">Read More</a>
-            </div>
-        </div>
-    </>
+    // <>
+    //     <div className="card news-item" >
+    //       <div className='card-thumbnail-img'>
+    //         <img src={props.imgUrl} className="card-img-top " alt="..."/>
+    //       </div>
+    //         <div className="card-body">
+    //             <h5 className="card-title">{props.title}</h5>
+    //             <p className="card-text">{props.description}</p>
+    //             <a href={props.newsUrl} className="btn btn-primary read-more-btn">Read More</a>
+    //         </div>
+    //     </div>
+    // </>
   )
 }
