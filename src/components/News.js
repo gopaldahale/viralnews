88f9c99ccf6d56fab01271c914ccd17495087876
Gopal Dahale <gopaldahale00@gmail.com>
2024-09-18 20:23:43 +0530

    new fecth

--- src/components/News.js
+++ src/components/News.js
@@ -11,26 +11,42 @@ export default function NewsCompo() {
     // const url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${process.env.REACT_APP_NEWS_API_KEY}`;
     // const url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=9cb45c816ff64e5993f4498a1564e2c1`;
     const apiKey = process.env.REACT_APP_NEWS_API_KEY;
-    console.log("API Key:", apiKey); 
+    console.log("API Key:", apiKey);
 
-    const url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${apiKey}`;
+    // const url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${apiKey}`;
 
-    fetch(url)
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-        return response.json();
-      })
-      .then((data) => {
-        setArticles(data.articles);
+    //   fetch(url)
+    //   .then((response) => {
+    //       if (!response.ok) {
+    //         throw new Error('Network response was not ok');
+    //       }
+    //       return response.json();
+    //     })
+    //     .then((data) => {
+    //       setArticles(data.articles);
+    //       setLoading(false);
+    //     })
+    //     .catch((error) => {
+    //       console.error('Error:', error);
+    //       setError(error);
+    //       setLoading(false);
+    //     });
+    // }, []);
+
+    // Fetch data when the component loads
+    const fetchData = async () => {
+      try {
+        const response = await fetch('https://newsapi.org/v2/top-headlines?country=us&apiKey=9cb45c816ff64e5993f4498a1564e2c1');
+        const data = await response.json();
+        setArticles(data.articles); // Assuming the API returns articles in a key called 'articles'
         setLoading(false);
-      })
-      .catch((error) => {
-        console.error('Error:', error);
-        setError(error);
+      } catch (error) {
+        console.error('Error fetching data:', error);
         setLoading(false);
-      });
+      }
+    };
+
+    fetchData();
   }, []);
 
   if (loading) {
@@ -52,7 +68,7 @@ export default function NewsCompo() {
                 imgUrl={article.urlToImage ? article.urlToImage : placeholderImage}
                 title={article.title.slice(0, 40)}
                 description={typeof article.description === 'string' ? (article.description.length > 40 ? article.description.slice(0, 88) + '...' : article.description) : ''}
-                newsUrl={article.url} 
+                newsUrl={article.url}
               />
             </div>
           ))
